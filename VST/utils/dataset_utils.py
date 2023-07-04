0e05940dc34cf147eaf6bc905d5e8d9ca9b44a8e
monisj <koolmonis@gmail.com>
2023-07-04 09:42:57 +0500

    Added Sign Language Detection Repo

---
+++ VST/utils/dataset_utils.py
@@ -0,0 +1,134 @@
+import os
+import time
+
+import pandas as pd
+import pickle as pkl
+from tqdm import tqdm
+
+from models.sign_model import SignModel
+from utils.landmark_utils import save_landmarks_from_video, load_array
+
+def load_dataset():
+    dataset = [
+        file_name.replace(".pickle", "").replace("lh_", "")
+        for root, dirs, files in os.walk(os.path.join("data", "dataset"))
+        for file_name in files
+        if file_name.endswith(".pickle") and file_name.startswith("lh_")
+    ]
+    return dataset
+    
+#load_dataset()
+
+def new_videos_load_dataset():
+    videos = [
+        file_name.replace(".mp4", "")
+        for root, dirs, files in os.walk(os.path.join("data", "videos"))
+        for file_name in files
+        if file_name.endswith(".mp4")
+    ]
+    dataset = [
+        file_name.replace(".pickle", "").replace("lh_", "")
+        for root, dirs, files in os.walk(os.path.join("data", "dataset"))
+        for file_name in files
+        if file_name.endswith(".pickle") and file_name.startswith("lh_")
+    ]
+
+    # Create the dataset from the reference videos
+    videos_not_in_dataset = list(set(videos).difference(set(dataset)))
+    n = len(videos_not_in_dataset)
+    if n > 0:
+        print(f"\nExtracting landmarks from new videos: {n} videos detected\n")
+
+        for idx in tqdm(range(n)):
+            save_landmarks_from_video(videos_not_in_dataset[idx])
+
+    return videos
+
+def save_embeds(arr, path):
+    file = open(path, "wb")
+    pkl.dump(arr, file)
+    file.close()
+
+
+def load_embeds(path):
+    file = open(path, "rb")
+    embed = pkl.load(file)
+    file.close()
+    return embed
+
+def load_reference_signs(videos):
+    print('loading references')
+    reference_signs = pd.DataFrame(columns=["name", "sign_model", "distance"])
+    #tstart=time.time()
+    for video_name in videos:
+        #vstart=time.time()
+        sign_name = video_name.split("_")[0]
+        path = os.path.join("data", "dataset", sign_name, video_name)
+        # lstart=time.time()
+        left_hand_list = load_array(os.path.join(path, f"lh_{video_name}.pickle"))
+        # lend=time.time()
+        # print(f'{video_name} left time=',(lend-lstart))
+        # rstart=time.time()
+        right_hand_list = load_array(os.path.join(path, f"rh_{video_name}.pickle"))
+        # rend=time.time()
+        # print(f'{video_name} left time=',(rend-rstart))
+
+        reference_signs = reference_signs.append(
+            {
+                "name": sign_name,
+                "sign_model": SignModel(left_hand_list, right_hand_list),
+                "distance": 0,
+            },
+            ignore_index=True,
+        )
+        # vend=time.time()
+    #     print(f'{video_name} time=',(vend-vstart))
+    # tend=time.time()
+    # print('total time=',(tend-tstart))
+    print(
+        f'Dictionary count: {reference_signs[["name", "sign_model"]].groupby(["name"]).count()}'
+    )
+    return reference_signs
+
+def new_load_reference_signs(videos,subject):
+    # print('loading references')
+    reference_signs = pd.DataFrame(columns=["name", "sign_model", "distance"])
+    #tstart=time.time()
+    for video_name in videos:
+        #vstart=time.time()
+        sign_name = video_name.split("_")[0]
+        path = os.path.join("data", f"{subject}_dataset", sign_name, video_name)
+        #data_path = os.path.join(path, video_name)
+
+        # lstart=time.time()
+        left_hand_list = load_array(os.path.join(path, f"lh_{video_name}.pickle"))
+        # lend=time.time()
+        # print(f'{video_name} left time=',(lend-lstart))
+        # rstart=time.time()
+        right_hand_list = load_array(os.path.join(path, f"rh_{video_name}.pickle"))
+        # rend=time.time()
+        # print(f'{video_name} left time=',(rend-rstart))
+        if not os.path.exists(os.path.join(path, f"embeddings_{video_name}.pickle")):
+            embeddings=SignModel(left_hand_list, right_hand_list)
+            save_embeds(embeddings,os.path.join(path, f"embeddings_{video_name}.pickle"))
+        else :
+            embeddings=load_embeds(os.path.join(path, f"embeddings_{video_name}.pickle"))
+
+        reference_signs = reference_signs.append(
+            {
+                "name": sign_name,
+                "sign_model": embeddings,
+                "distance": 0,
+            },
+            ignore_index=True,
+        )
+        # vend=time.time()
+    #     print(f'{video_name} time=',(vend-vstart))
+    # tend=time.time()
+    # print('total time=',(tend-tstart))
+    # print(
+    #     f'Dictionary count: {reference_signs[["name", "sign_model"]].groupby(["name"]).count()}'
+    # )
+    return reference_signs
+
+
---
+++ VST/webcam_manager.py
@@ -0,0 +1,165 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+
+
+WHITE_COLOR = (245, 242, 226)
+RED_COLOR = (25, 35, 240)
+
+HEIGHT = 600
+
+
+class WebcamManager(object):
+    """Object that displays the Webcam output, draws the landmarks detected and
+    outputs the sign prediction
+    """
+
+    def __init__(self):
+        self.sign_detected = ""
+        self.sign=[" "]
+        self.dist=[" "]
+        self.val=""
+
+    def update(
+        self, frame: np.ndarray, results, sign_detected, is_recording,sign,dist,val
+    ):
+        self.sign_detected = sign_detected
+        self.sign=sign
+        self.dist=dist
+        self.val=val
+        # Draw landmarks
+        self.draw_landmarks(frame, results)
+
+        WIDTH = int(HEIGHT * len(frame[0]) / len(frame))
+        # Resize frame
+        frame = cv2.resize(frame, (WIDTH, HEIGHT), interpolation=cv2.INTER_AREA)
+
+        # Flip the image vertically for mirror effect
+        frame = cv2.flip(frame, 1)
+
+        # Write result if there is
+        frame,acc = self.draw_text(frame)
+
+        # Chose circle color
+        color = WHITE_COLOR
+        if is_recording:
+            color = RED_COLOR
+
+        # Update the frame
+        cv2.circle(frame, (30, 30), 20, color, -1)
+        #cv2.imshow("OpenCV Feed", frame)
+        return frame,acc
+
+    def draw_text(
+        self,
+        frame,
+        font=cv2.FONT_HERSHEY_COMPLEX,
+        font_size=1,
+        font_thickness=2,
+        offset=int(HEIGHT * 0.02),
+        bg_color=(245, 242, 176, 0.85),
+    ):
+        acc2=0
+        window_w = int(HEIGHT * len(frame[0]) / len(frame))
+        list1=[]
+        if self.sign_detected==self.val:
+            self.sign_detected=f'Predicted Correctly With Accuracy ={self.dist[0]}'
+            for i in range(len(self.sign)):
+                if self.sign[i]==self.val:
+                    list1.append(self.dist[i])
+                    if len(list1)==2:
+                        acc1=int(list1[0])
+                        acc2=int(list1[1])
+                        acc1=((acc1-500)/500)*100
+                        acc2=((acc2-500)/500)*100
+                        if int(list1[0])<500 or int(list1[1])<500:
+                            self.sign_detected='Predicted correctly With Accuracy =95'
+                            acc2=95
+                            break
+                        else:
+                            if acc1>100 or acc2>100:
+                                acc=((acc1+acc2)/2)//100
+                                self.sign_detected=f'Predicted correctly With Accuracy ={acc}'
+                                acc2=acc
+                                break
+                            else:
+                                acc=(acc1+acc2)/2
+                                self.sign_detected=f'Predicted correctly With Accuracy ={100-acc}'
+                                acc2=100-acc
+                                break
+        elif self.sign_detected !=self.val:
+            for i in range(len(self.sign)):
+                if self.sign[i]==self.val:
+                    list1.append(self.dist[i])
+                    if len(list1)==2:
+                        if list1[0]==float('inf'):
+                            self.sign_detected='No Sign Detected'
+                            acc2=0
+                        else:    
+                            acc1=int(list1[0])
+                            acc2=int(list1[1])
+                            acc1=((acc1-500)/500)*100
+                            acc2=((acc2-500)/500)*100
+                            if int(list1[0])<500 or int(list1[1])<500:
+                                self.sign_detected='Predicted Incorrectly With Accuracy =95'
+                                acc2=95
+                                break
+                            else:
+                                if acc1>100 or acc2>100:
+                                    acc=((acc1+acc2)/2)//100
+                                    self.sign_detected=f'Predicted Incorrectly With Accuracy ={acc}'
+                                    acc2=acc
+                                    break
+                                else:
+                                    acc=(acc1+acc2)//2
+                                    self.sign_detected=f'Predicted Incorrectly With Accuracy ={100-acc}'
+                                    acc2=100-acc
+                                    break
+
+        (text_w, text_h), _ = cv2.getTextSize(
+            self.sign_detected, font, font_size, font_thickness
+        )
+
+        text_x, text_y = int((window_w - text_w) / 2), HEIGHT - text_h - offset
+
+        cv2.rectangle(frame, (0, text_y - offset), (window_w, HEIGHT), bg_color, -1)
+        cv2.putText(
+            frame,
+            self.sign_detected,
+            (text_x, text_y + text_h + font_size - 1),
+            font,
+            1,
+            (200, 50, 37),
+            font_thickness,
+        )
+        return frame,acc2
+
+    @staticmethod
+    def draw_landmarks(image, results):
+        mp_holistic = mp.solutions.holistic  # Holistic model
+        mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
+
+        # Draw left hand connections
+        mp_drawing.draw_landmarks(
+            image,
+            landmark_list=results.left_hand_landmarks,
+            connections=mp_holistic.HAND_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing.DrawingSpec(
+                color=(232, 254, 255), thickness=1, circle_radius=1
+            ),
+            connection_drawing_spec=mp_drawing.DrawingSpec(
+                color=(255, 249, 161), thickness=2, circle_radius=2
+            ),
+        )
+        # Draw right hand connections
+        mp_drawing.draw_landmarks(
+            image,
+            landmark_list=results.right_hand_landmarks,
+            connections=mp_holistic.HAND_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing.DrawingSpec(
+                color=(232, 254, 255), thickness=1, circle_radius=2
+            ),
+            connection_drawing_spec=mp_drawing.DrawingSpec(
+                color=(255, 249, 161), thickness=2, circle_radius=2
+            ),
+        )
