7e8558c4bdd80326b9ea40b06adf8bccf95191c4
monisj <koolmonis@gmail.com>
2023-07-04 11:58:03 +0500

    Face Detection Now Running

--- Face_landmark.py
+++ Face_landmark.py
@@ -1,54 +1,45 @@
-
-import cv2
+import time
+import cv2 as cv
 import mediapipe as mp
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-
-my_drawing_specs = mp_drawing.DrawingSpec(color = (0, 255, 0), thickness = 1)
-cap = cv2.VideoCapture(0)
-
-mp_face_mesh = mp.solutions.face_mesh
-
-with mp_face_mesh.FaceMesh(
-        max_num_faces = 1,
-        min_detection_confidence = 0.5,
-        min_tracking_confidence = 0.5
-    ) as face_mesh:
-    
-
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
+import numpy as np
+mp_face_detection = mp.solutions.face_detection
+cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+with mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.5) as face_detector:
+    frame_counter = 0
+    fonts = cv.FONT_HERSHEY_PLAIN
+    start_time = time.time()
+    while True:
+        frame_counter += 1
+        ret, frame = cap.read()
+        if ret is False:
             break
-            
-            
-        results = face_mesh.process(image)
+        rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+        results = face_detector.process(rgb_frame)
+        frame_height, frame_width, c = frame.shape
+        if results.detections:
+            for face in results.detections:
+                face_react = np.multiply(
+                    [
+                        face.location_data.relative_bounding_box.xmin,
+                        face.location_data.relative_bounding_box.ymin,
+                        face.location_data.relative_bounding_box.width,
+                        face.location_data.relative_bounding_box.height,
+                    ],
+                    [frame_width, frame_height, frame_width, frame_height]).astype(int)
+                
+                cv.rectangle(frame, face_react, color=(255, 255, 255), thickness=2)
+                key_points = np.array([(p.x, p.y) for p in face.location_data.relative_keypoints])
+                key_points_coords = np.multiply(key_points,[frame_width,frame_height]).astype(int)
+                for p in key_points_coords:
+                    cv.circle(frame, p, 4, (255, 255, 255), 2)
+                    cv.circle(frame, p, 2, (0, 0, 0), -1)
         
-        for face_landmarks in results.multi_face_landmarks:
-            mp_drawing.draw_landmarks(
-                image = image,
-                landmark_list = face_landmarks,
-                connections = mp_face_mesh.FACEMESH_TESSELATION,
-                landmark_drawing_spec = None,
-                connection_drawing_spec = mp_drawing_styles
-                .get_default_face_mesh_tesselation_style()
-            )
-            
-            mp_drawing.draw_landmarks(
-                image = image,
-                landmark_list = face_landmarks,
-                connections = mp_face_mesh.FACEMESH_CONTOURS,
-                landmark_drawing_spec = None,
-                connection_drawing_spec = my_drawing_specs
-                 .get_default_face_mesh_contours_style()
-            )
-
-
-        cv2.imshow("My video capture", cv2.flip(image, 1))
-
-        if cv2.waitKey(100) & 0xFF == ord('q'):
+        fps = frame_counter / (time.time() - start_time)
+        cv.putText(frame,f"FPS: {fps:.2f}",(30, 30),cv.FONT_HERSHEY_DUPLEX,0.7,(0, 255, 255),2,)
+        cv.imshow("frame", frame)
+        key = cv.waitKey(1)
+        if key == ord("q"):
             break
-        
-cap.release()
-cv2.destroyAllWindows()
- 
+    cap.release()
+    cv.destroyAllWindows() 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
-ube
+with mp_face_mesh.FaceMesh(
+    static_image_mode=True,
+    max_num_faces=1,
+    refine_landmarks=True,
+    min_detection_confidence=0.5) as face_mesh:
